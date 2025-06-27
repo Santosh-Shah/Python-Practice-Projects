7d1f4c00b2146cbbf58c67be236cb0a6bff28778
Santosh Shah <santoshofficial679@gmail.com>
2025-06-27 12:13:44 +0545

    Contact Book System is created with simple features

--- Contact-Book-System/v1/main.py
+++ Contact-Book-System/v1/main.py
@@ -1,6 +1,41 @@
 
 
+# step three-----------------------------------------------
+contact_list = []
+def add_contact():
+  name = input("Enter you name: ").strip()
+  phone = input("Enter you mobile number: ").strip()
+  email = input("Enter you email address: ").strip()
 
+  contacts = {
+    "name": name,
+    "phone": phone,
+    "email": email
+  }
+
+  contact_list.append(contacts)
+  print(f"========={name} :added successfully! Congrats===========")
+
+# View Contact Method-------------------------------------
+def view_contact():
+  print("====================List of the Constact===================")
+  for contact in contact_list:
+    print(f"Name: {contact.get('name')}   ||    Mobile No. {contact.get('phone')}    ||    Email Address: {contact.get('email')}")
+
+
+# Delete Contact Method-------------------------------------------------
+def delete_contact():
+  deletable_name = input("Enter name to delete: ").strip()
+  for contact in contact_list:
+    if contact.get('name').lower() == deletable_name.lower():
+      contact_list.remove(contact)
+      print(f"Congrats! {deletable_name} is deleted successfully\n")  
+      return
+  
+  print(f"Sorry! {deletable_name} not found")
+
+
+# step two
 def show_menu():
   print("=============Contact Book Menu===============")
   print("1 - Add Contact")
@@ -22,5 +57,6 @@ while True:
     delete_contact()
   elif chosenNum == 4:
     print("Exiting Book, GoodBye!")
+    break
   else:
     print("Invalid choise, please try again leter!")
